f87c0ab61c8858e85fc4b5538f67e560a7327291
jegex <hujan008@gmail.com>
2026-05-31 03:24:26 +0700

    refactor: use priceable_currency_model() helper in PriceFactory

--- database/factories/PriceFactory.php
+++ database/factories/PriceFactory.php
@@ -3,8 +3,8 @@
 namespace Jegex\LaravelPriceable\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Jegex\LaravelPriceable\Models\Currency;
 use Jegex\LaravelPriceable\Models\Price;
+use function Jegex\LaravelPriceable\priceable_currency_model;
 
 class PriceFactory extends Factory
 {
@@ -13,7 +13,7 @@ class PriceFactory extends Factory
     public function definition(): array
     {
         return [
-            'currency_id' => Currency::factory(),
+            'currency_id' => priceable_currency_model()::factory(),
             'price' => $this->faker->numberBetween(100, 99999),
             'compare_price' => null,
             'min_quantity' => 1,
